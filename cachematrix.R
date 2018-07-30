54852b7b237b8592996e49e6da306ad443b4894c
WILLIAM MASSALINO <41436246+WilliamMassalino@users.noreply.github.com>
2018-07-30 09:55:25 -0300

    Update Cachematrix.R
    A função "makeCacheMatrix" cria um objeto de matriz especial e, em seguida, a função "cacheSolve" calcula o inverso da matriz. Se o inverso da Matriz já foi calculado, ele é encontrado no cache e o retorna, sem necessidade de recalculá-lo.

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,33 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        inverseX <- NULL
+        set <- function(y){
+        x<<- y
+        inverseX <<-NULL
 }
-
+        get <- function()x
+        setinverse <- function(inverse)inverseX <<-inverse
+        getinverse <- functioin()inverseX
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+        
+}
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+inverseX <- x$getinverse()
+        if(!is.null(inverseX)){
+message("Getting cached data.")
+                return(inverseX)
+}
+        data <- x$get()
+        inverseX <- solve(data)
+        x$setinverse(inverseX)
+        inverseX
+
 }
